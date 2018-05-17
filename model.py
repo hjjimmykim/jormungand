e5ac6abdc40377391fdc31088a84a2724092479c
HJK <hjjimmykim@gmail.com>
2018-05-17 16:47:44 -0500

    Minor fix

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 class Net:
     def __init__(self, inputs, labels, hidden_dim=50):
@@ -8,11 +9,11 @@ class Net:
         output_dim = int(labels.shape[1])
     
         # Weight initializations
-        W1 = tf.Variable(tf.truncated_normal([input_dim,hidden_dim],std=0.1))
-        b1 = tf.Variable(tf.constant(0.1,shape=[hidden_dim])
+        W1 = tf.Variable(tf.truncated_normal([input_dim,hidden_dim],stddev=0.1))
+        b1 = tf.Variable(tf.constant(0.1,shape=[hidden_dim]))
         
-        W2 = tf.Variable(tf.truncated_normal([hidden_dim,output_dim],std=0.1))
-        b2 = tf.Variable(tf.constant(0.1,shape=[output_dim])
+        W2 = tf.Variable(tf.truncated_normal([hidden_dim,output_dim],stddev=0.1))
+        b2 = tf.Variable(tf.constant(0.1,shape=[output_dim]))
         
         # Hidden layer
         h1 = tf.nn.relu(tf.matmul(self.x,W1) + b1)
@@ -38,17 +39,21 @@ class Net:
         # L2
         if not hasattr(self, "loss_L2"):
             self.loss_L2 = self.loss_vanilla
-            
-        for i in range(len(self.params)):
-            self.loss_L2 += (lambda_L2/2) * tf.reduce_sum(tf.square(self.params[i]-self.params_prev[i]))
+        else:
+            for i in range(len(self.params)):
+                self.loss_L2 += (lambda_L2/2) * tf.reduce_sum(tf.square(self.params[i]-self.params_prev[i]))
+        self.step = tf.train.GradientDescentOptimizer(0.1).minimize(self.loss_L2)
         
     def set_EWC(self, lambda_EWC):
         # Elastic weight consolidation
         if not hasattr(self, "loss_EWC"):
             self.loss_EWC = self.loss_vanilla
-            
-        for i in range(len(self.params)):
-            self.loss_EWC += (lambda_EWC/2) * tf.reduce_sum(tf.multiply(self.Fisher[i].astype(np.float32),tf.square(self.params[i]-self.params_prev[i])))
+        else:
+            print('blah_EWC')
+            # Summing up losses
+            for i in range(len(self.params)):
+                self.loss_EWC += (lambda_EWC/2) * tf.reduce_sum(tf.multiply(self.Fisher[i].astype(np.float32),tf.square(self.params[i]-self.params_prev[i])))
+        self.step = tf.train.GradientDescentOptimizer(0.1).minimize(self.loss_EWC)
         
     def save_parameters(self):
         # Save parameters after a training run
@@ -60,7 +65,7 @@ class Net:
         # Restore parameters for previous task
         if hasattr(self, "params_prev"):
             for i in range(len(self.params)):
-                sess.run(self.parameters[i].assign(self.params_prev[i]))
+                sess.run(self.params[i].assign(self.params_prev[i]))
                 
     def compute_Fisher(self, data, sess, sample_size=200):
         # Compute Fisher Info. for EWC
@@ -68,7 +73,7 @@ class Net:
         # Initialize
         self.Fisher = []
         for i in range(len(self.params)):
-            self.Fisher.append(np.zeros(self.params[i].shape))
+            self.Fisher.append(np.zeros(self.params[i].get_shape().as_list()))
             
         # Sample random class
         probs = tf.nn.softmax(self.outputs)
@@ -78,10 +83,10 @@ class Net:
             # Sample random data point
             i_data = np.random.randint(data.shape[0])
             # First derivative
-            log_L_grad = sess.run(tf.gradients(tf.log(probs[0,class_ind]),self.params), feed_dict={self.x: data[i_data:i_data+1]})
+            log_L_grad = sess.run(tf.gradients(tf.log(probs[0,output_sample]),self.params), feed_dict={self.x: data[i_data:i_data+1]})
             # Squared and added
-            for i in range(len(self.Fisher)):
-                self.Fisher[i] += np.square(log_L_grad)
+            for j in range(len(self.Fisher)):
+                self.Fisher[j] += np.square(log_L_grad[j])
                 
         # Average
         for i in range(len(self.Fisher)):
--- train.py
+++ train.py
@@ -1,15 +1,62 @@
-def train(model, trainset, testsets, lambda_L2, lambda_EWC, N_it, batch_size, ep_rec):
-    model.restore(sess) # Restore optimal weights from previous session
+import numpy as np
+import time
+
+def train(sess, model, trainset, testsets, inputs, outputs, N_it, batch_size, type, lamb, ep_rec, ep_time):
+    # sess = tensorflow session
+    # model = network
+    # trainset = dataset
+    # testsets = datasets for different tasks
+    # inputs = input placeholder
+    # outputs = output placeholder
+    # N_it = number of iterations (batches)
+    # batch_size = size of batch
+    # type = 0: Vanilla, 1: L2, 2: EWC
+    # lamb = regularization parameter for L2 and EWC
+    # ep_rec = record test accuracy every ep_rec iterations
+    # ep_time = display runtime every ep_time iterations
     
-    # Vanilla
-    model.set_vanilla()
+    # test_acc_list = list containing test accuracies for each task
     
-    # L2
-    model.set_L2()
-    
-    # EWC
-    model.set_EWC()
-    
+    N_task = len(testsets) # Number of tasks
+
+    model.load_parameters(sess) # Restore optimal weights from previous session
+
+    if type == 0:
+        model.set_vanilla() # Vanilla
+        name = 'Vanilla'
+    elif type == 1:
+        model.set_L2(lamb) # L2
+        name = 'L2'
+    elif type == 2:
+        model.set_EWC(lamb) # EWC
+        name = 'EWC'
+        
+    # Initialize test accuracy arrays
+    test_acc_list = []
+    for i in range(N_task):
+        test_acc_list.append(np.zeros(int(N_it/ep_rec)))
+
+    #print(name + ' training started...')
+    time_start = time.time()
+    time_p1 = time.time()
     for i_it in range(N_it):
         batch = trainset.train.next_batch(batch_size)
+        model.step.run(feed_dict={inputs: batch[0], outputs: batch[1]})
         
+        # Record test accuracies
+        if i_it % ep_rec == 0:
+            for i_task in range(N_task):
+                feed_dict = {inputs: testsets[i_task].test.images, outputs:testsets[i_task].test.labels}
+                test_acc_list[i_task][int(i_it/ep_rec)] = model.acc.eval(feed_dict=feed_dict)
+                
+        # Record time
+        if i_it % ep_time == 0 and i_it != 0:
+            time_p2 = time.time()
+            print('Runtime for iterations ' + str(i_it-ep_time) + '-' + str(i_it) + ': ' + str(time_p2-time_p1) + ' s')
+            time_p1 = time_p2
+    time_finish = time.time()
+    print(name +' training runtime: ' + str(time_finish-time_start) + ' s')
+    #print(name + ' training finished...')
+
+    return test_acc_list
+    
